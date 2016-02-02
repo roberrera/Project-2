1539202488948ae2b444765cee4f1b9c1f7fb1e6
roberrera <roberterrera@gmail.com>
2016-02-02 16:26:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/roberrera/project2/Classes/Neighborhood.java
@@ -0,0 +1,7 @@
+package com.example.roberrera.project2.Classes;
+
+/**
+ * Created by Rob on 2/2/16.
+ */
+public class Neighborhood {
+}
---
+++ app/src/main/java/com/example/roberrera/project2/Classes/NeighborhoodSQLOpenHelper.java
@@ -0,0 +1,12 @@
+package com.example.roberrera.project2.Classes;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Rob on 2/2/16.
+ */
+public class NeighborhoodSQLOpenHelper extends SQLiteOpenHelper {
+    
+
+
+}
